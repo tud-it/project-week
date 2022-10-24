2fdcf571ec752b95cd1756318104fc3656bd37d3
Richardrasch <richard.rasch@tu-dresden.de>
2022-10-24 13:29:39 +0200

    update

--- guischiffe.py
+++ guischiffe.py
@@ -1,19 +1,17 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 
 
-class Main( Frame ):
-    def __init__( self ):
-        tk.Frame.__init__(self)
-        self.geometry = Tk(className="Schiffe versenken").geometry("700x700")
-        self.master.title("Schiffe versenken")
-        self.closebutton = Button(self, text="close", width = 25, command = self.close_window)
-        self.closebutton.grid(row = 1, column = 1, columnspan = 2)
-    def close_window(self):
-        self.destroy()
 def main():
-    window= Main()
-    #window.geometry("700x700")
+    window= tk.Tk(className="Schiffe versenken")
+    window.geometry("700x700")
+    window.resizable(False,False)
+    placebutton = ttk.Button(window,text="Schiffe Platzieren",command = "")
+    placebutton.pack(ipadx=2, ipady=5)
+    field = ttk.Frame(window, height=20, width=20, borderwidth=1,relief=SOLID)
+    field.anchor("center")
+    field.pack()
     window.mainloop()
 if __name__ == '__main__':
     main()
