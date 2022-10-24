804a1a65078e39ed11da73a0ea1026f83be7f2ac
floork <floork@gmx.de>
2022-10-24 13:37:05 +0200

    shoting function

--- shoot.py
+++ shoot.py
@@ -1,15 +1,22 @@
 """shoot.py"""
+
 import random
 
+from dictionary import board
 
-def schuss(dic, count: int):
+
+def schuss(dict, count: int):
     """function to shoot a boat"""
-    schiff_b = random.choice("A","B", "C", "D", "F", "G", "H", "I", "J")
-    schiff_z = random.int(0, 9)
-    schiff = tuple(schiff_b, schiff_z)
-    if dic.key(schiff):
-        hit = dic.key(schiff) = False
+    field_list = list(board.keys())
+    field = random.choice(field_list)
+
+    # empty list to store already shot field
+    already_shot = list()
+
+    if field in already_shot:
+        schuss(count)
+    if dict.key(field):
+        hit = dict.key(field) = False
         count += 1
         return hit, count
-    else:
-        ...
+    return print("Nicht Getroffen!")
