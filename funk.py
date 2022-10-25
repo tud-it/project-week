b43f46207c3769f0d8c21d62d0fcef96cf94454a
Florian Herold <flhe628e@tu-dresden.de>
2022-10-25 10:59:22 +0200

    add

--- funk.py
+++ funk.py
@@ -7,10 +7,8 @@ def print_all_fields():
     """print all the fields"""
     brd = create_board(SPIELFELDGRÖSSE)
     feld = list(brd.keys())
-    donelist: liste = []
     for elem in feld:
         num_string = str(elem[1])
         final_string = elem[0]+num_string
-        donelist.append(final_string)
-        print(final_string)
+        return final_string
 print_all_fields()
